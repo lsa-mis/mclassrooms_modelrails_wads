1ccc94307ae2c20b41e68b0874f963701ea005d5
Dave Chmura <dschmura@humbledaisy.com>
2026-03-25 08:36:56 -0400

    chore(deps): Configure test infrastructure
    Add RSpec, FactoryBot, Capybara, Playwright, SimpleCov, letter_opener,
    and hotwire-spark. Add OmniAuth providers and Pwned for auth phase.
    Configure Action Mailer with letter_opener for development.

---
+++ spec/support/capybara.rb
@@ -0,0 +1,14 @@
+require "capybara/playwright"
+
+Capybara.register_driver :playwright do |app|
+  Capybara::Playwright::Driver.new(app, browser_type: :chromium, headless: true)
+end
+
+Capybara.default_driver = :rack_test
+Capybara.javascript_driver = :playwright
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :playwright
+  end
+end
