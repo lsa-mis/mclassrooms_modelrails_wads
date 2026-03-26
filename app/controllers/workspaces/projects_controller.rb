7ad459a16e60554ca4958c768ed6e1dae086d484
Dave Chmura <dschmura@humbledaisy.com>
2026-03-26 17:53:31 -0400

    feat(projects): Add Projects CRUD with creator auto-assignment and max_projects enforcement

---
+++ app/controllers/workspaces/projects_controller.rb
@@ -0,0 +1,64 @@
+module Workspaces
+  class ProjectsController < ApplicationController
+    include WorkspaceScoped
+    before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+    def index
+      authorize Project
+      @projects = @workspace.projects.kept
+    end
+
+    def new
+      authorize Project
+      @project = @workspace.projects.build
+    end
+
+    def create
+      authorize Project
+      @project = @workspace.projects.build(project_params)
+      @project.created_by = Current.user
+
+      if @project.save
+        @project.project_memberships.create!(user: Current.user, role: "creator")
+        redirect_to workspace_project_path(@workspace, @project), notice: t(".success")
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      authorize @project
+    end
+
+    def edit
+      authorize @project
+    end
+
+    def update
+      authorize @project
+      if @project.update(project_params)
+        redirect_to workspace_project_path(@workspace, @project), notice: t(".success")
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      authorize @project
+      @project.discard!
+      redirect_to workspace_projects_path(@workspace), notice: t(".success")
+    end
+
+    private
+
+    def set_project
+      @project = @workspace.projects.kept.find_by!(slug: params[:slug])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to workspace_projects_path(@workspace), alert: t("projects.not_found")
+    end
+
+    def project_params
+      params.require(:project).permit(:name, :description, :primary_color)
+    end
+  end
+end
