a772e8fcea1f86fa35309547fe5f3c0b04da562e
Dave Chmura <dschmura@humbledaisy.com>
2026-03-26 19:02:57 -0400

    test: add policy specs for BrandingPolicy and SettingsPolicy

---
+++ spec/policies/workspaces/settings_policy_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe Workspaces::SettingsPolicy do
+  let(:workspace) { create(:workspace) }
+
+  before { Current.workspace = workspace }
+
+  describe "for owner" do
+    let(:user) { create(:user) }
+    before { create(:membership, :owner, user: user, workspace: workspace) }
+
+    it "allows edit" do
+      expect(described_class.new(user, workspace).edit?).to be true
+    end
+
+    it "allows update" do
+      expect(described_class.new(user, workspace).update?).to be true
+    end
+  end
+
+  describe "for member" do
+    let(:user) { create(:user) }
+    before { create(:membership, user: user, workspace: workspace) }
+
+    it "denies edit" do
+      expect(described_class.new(user, workspace).edit?).to be false
+    end
+
+    it "denies update" do
+      expect(described_class.new(user, workspace).update?).to be false
+    end
+  end
+end
